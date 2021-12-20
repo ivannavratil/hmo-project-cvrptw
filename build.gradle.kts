4844cef6637fd87d82ef6a896c6613bd09fdfd1b
Ivan Navratil <ivan.navratil@porsche.digital>
2021-12-20 20:33:48 +0100

    Inital commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "fer.hmo"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
