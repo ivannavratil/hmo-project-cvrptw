e5afa80a5d0fc584910a603e5a0ba03f5c52bcef
Ivan Navratil <ivan.navratil@porsche.digital>
2021-12-27 23:07:26 +0100

    Print removed from main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,13 @@
-import Distances.inverseDistances
-
 // TODO Remove all F64Array if calculations are not vectorized.
 // TODO Array / ArrayList / List ?
 // TODO Sparse structures?
 
 fun main() {
-    val instanceId = 1
+    val instanceId = 6
     val instance = Instance.fromInstanceId(instanceId)
 
     val aco = AntColony(instance, startingTemperature = 100.0)
 
-    for (i in instance.nodes.indices) {
-        for (j in instance.nodes.indices)
-            print(String.format("%6.2f", inverseDistances[i, j]))
-        println()
-    }
-
-
-    aco.run(800)
+    aco.run(100)
     Solution.fromSolutionBuilder(aco.incumbentSolution!!).exportToFile("src/main/resources/results/i$instanceId.txt")
 }
