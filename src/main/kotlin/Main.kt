754b9554117874dd6e7d4177b26e256a274a27b3
LMesaric <luka.mesaric@fer.hr>
2021-12-27 20:22:16 +0100

    Reorder initialization

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,14 +8,13 @@ fun main() {
     val instanceId = 1
     val instance = Instance.fromInstanceId(instanceId)
 
-    val aco = AntColony(instance, startingTemperature = 100.0)
-
     for (i in instance.nodes.indices) {
         for (j in instance.nodes.indices)
             print(String.format("%6.2f", inverseDistances[i, j]))
         println()
     }
 
+    val aco = AntColony(instance, startingTemperature = 100.0)
     aco.run(800)
     Solution.fromSolutionBuilder(aco.incumbentSolution!!).exportToFile("src/main/resources/results/test.txt")
 }
