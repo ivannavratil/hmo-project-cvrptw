2641a6aa2adce092d2c86f270bb3d3dc4403f662
LMesaric <luka.mesaric@fer.hr>
2021-12-29 00:54:22 +0100

    Complicate if-else

--- src/main/kotlin/heuristics/WaitHeuristic.kt
+++ src/main/kotlin/heuristics/WaitHeuristic.kt
@@ -8,10 +8,10 @@ object WaitHeuristic {
 
     fun calculateWaitTime(sourceNodeMeta: NodeMeta, destination: Node): Double {
         val arrivalTime = sourceNodeMeta.departureTime + calculateTravelTime(sourceNodeMeta.node.id, destination.id)
-        if (destination.readyTime <= arrivalTime) {
-            return 1.0
+        return when {
+            destination.readyTime <= arrivalTime -> 1.0
+            else -> 1.0 / (destination.readyTime - arrivalTime)
         }
-        return 1.0 / (destination.readyTime - arrivalTime)
     }
 
 }
