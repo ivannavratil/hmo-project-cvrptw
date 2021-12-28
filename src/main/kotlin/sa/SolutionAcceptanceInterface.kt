b229e017bf334403fe4d1cd64317e7f01458577d
LMesaric <luka.mesaric@fer.hr>
2021-12-28 20:00:30 +0100

    Rename parameters, fix bug in exponent

--- src/main/kotlin/sa/SolutionAcceptanceInterface.kt
+++ src/main/kotlin/sa/SolutionAcceptanceInterface.kt
@@ -1,25 +1,23 @@
 package sa
 
-import kotlin.math.abs
 import kotlin.math.exp
 
 interface SolutionAcceptanceInterface {
-    fun acceptSolution(current: Int, possible: Int, temperature: Double): Boolean
+    fun acceptSolution(current: Double, neighbor: Double, temperature: Double): Boolean
 }
 
 class StandardAcceptance : SolutionAcceptanceInterface {
-    override fun acceptSolution(current: Int, possible: Int, temperature: Double): Boolean {
-        if (possible > current) return true
-
-        val probabilityToAccept = exp((-abs(current - possible)) / temperature)
-
-        return probabilityToAccept > Math.random()
+    override fun acceptSolution(current: Double, neighbor: Double, temperature: Double): Boolean {
+        if (neighbor < current)
+            return true
+        val probabilityToAccept = exp((current - neighbor) / temperature)
+        return Math.random() < probabilityToAccept
     }
 }
 
 class Standard2Acceptance : SolutionAcceptanceInterface {
-    override fun acceptSolution(current: Int, possible: Int, temperature: Double): Boolean {
-        val probabilityToAccept = 1 / (1 + exp((current - possible) / temperature))
-        return probabilityToAccept > Math.random()
+    override fun acceptSolution(current: Double, neighbor: Double, temperature: Double): Boolean {
+        val probabilityToAccept = 1.0 / (1 + exp((neighbor - current) / temperature))
+        return Math.random() < probabilityToAccept
     }
 }
