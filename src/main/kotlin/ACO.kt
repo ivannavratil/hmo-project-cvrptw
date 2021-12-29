16f588471f40923ec6513f8432ca8c291281b10f
Ivan Navratil <ivan.navratil@porsche.digital>
2021-12-29 19:50:01 +0100

    Overwrite problem fixed

--- src/main/kotlin/ACO.kt
+++ src/main/kotlin/ACO.kt
@@ -12,6 +12,7 @@ import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.concurrent.thread
+import kotlin.random.Random
 import kotlin.system.measureTimeMillis
 
 // TODO Remove all F64Array if calculations are not vectorized.
@@ -22,7 +23,7 @@ fun copy(c: Config): Config = c.copy(ant = c.ant.copy(), antColony = c.antColony
 fun main() {
 
     val base = Config(
-        6, 1000,
+        6, Int.MAX_VALUE,
         Config.Ant(69, 0.7, 1.1, 3.0, 0.5, 0.25, 0.08),
         Config.AntColony(tauZero = 0.0001)
     )
@@ -33,64 +34,62 @@ fun main() {
 
     //TODO: i1 - tau= 0.001
 
-    for (i in 0..5) {
-
-        thread {
-            val cAlpha = copy(base)
-            for (alpha in mutableListOf(0.6, 0.65, 0.7, 0.75, 0.8)) {
-                cAlpha.ant.alpha = alpha
-                main2(cAlpha, "alpha", alpha)
-            }
-        }
-
-        thread {
-            val cBeta = copy(base)
-            for (beta in mutableListOf(0.5, 1.0, 1.25, 1.5, 1.75, 2.1, 2.5)) {
-                cBeta.ant.beta = beta
-                main2(cBeta, "beta", beta)
-            }
-        }
-
-//    thread {
-//        val cCount = copy(base)
-//        for (count in (10 until 100 step 10)) {
-//            cCount.ant.count = count
-//            main2(cCount, "count", count.toDouble())
-//        }
-//    }
-
-        thread {
-            val cqo = copy(base)
-            for (q0 in mutableListOf(0.05, 0.10, 0.15, 0.20, 0.25, 0.35)) {
-                cqo.ant.q0 = q0
-                main2(cqo, "q0", q0)
-            }
-        }
-
-        thread {
-            val cRho = copy(base)
-            for (rho in mutableListOf(0.001, 0.01, 0.03, 0.05, 0.1, 0.2)) {
-                cRho.ant.rho = rho
-                main2(cRho, "rho", rho)
-            }
-        }
-
-//    thread {
-//        val cTau = copy(base)
-//        for (tau in mutableListOf(1E-6, 1E-5, 1E-4, 5E-3, 1E-3, 1E-2, 1E-1)) {
-//            cTau.antColony.tauZero = tau
-//            main2(cTau, "tau", tau)
-//        }
-//    }
-
-        thread {
-            val cTheta = copy(base)
-            for (theta in mutableListOf(0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8)) {
-                cTheta.ant.theta = theta
-                main2(cTheta, "theta", theta)
-            }
+    thread {
+        val cAlpha = copy(base)
+        for (alpha in mutableListOf(0.6, 0.65, 0.7, 0.75, 0.8)) {
+            cAlpha.ant.alpha = alpha
+            main2(cAlpha, "alpha", alpha)
         }
     }
+
+    thread {
+        val cBeta = copy(base)
+        for (beta in mutableListOf(0.5, 1.0, 1.25, 1.5, 1.75, 2.1, 2.5)) {
+            cBeta.ant.beta = beta
+            main2(cBeta, "beta", beta)
+        }
+    }
+
+    thread {
+        val cCount = copy(base)
+        for (count in (10 until 100 step 10)) {
+            cCount.ant.count = count
+            main2(cCount, "count", count.toDouble())
+        }
+    }
+
+    thread {
+        val cqo = copy(base)
+        for (q0 in mutableListOf(0.05, 0.10, 0.15, 0.20, 0.25, 0.35)) {
+            cqo.ant.q0 = q0
+            main2(cqo, "q0", q0)
+        }
+    }
+
+    thread {
+        val cRho = copy(base)
+        for (rho in mutableListOf(0.001, 0.01, 0.03, 0.05, 0.1, 0.2)) {
+            cRho.ant.rho = rho
+            main2(cRho, "rho", rho)
+        }
+    }
+
+    thread {
+        val cTau = copy(base)
+        for (tau in mutableListOf(1E-6, 1E-5, 1E-4, 5E-3, 1E-3, 1E-2, 1E-1)) {
+            cTau.antColony.tauZero = tau
+            main2(cTau, "tau", tau)
+        }
+    }
+
+    thread {
+        val cTheta = copy(base)
+        for (theta in mutableListOf(0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8)) {
+            cTheta.ant.theta = theta
+            main2(cTheta, "theta", theta)
+        }
+    }
+
 }
 
 fun main2(config: Config, param: String, paramValue: Double) {
@@ -111,7 +110,7 @@ fun main2(config: Config, param: String, paramValue: Double) {
     val formattedTimestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss"))
 
     Solution.fromSolutionBuilder(aco.incumbentSolution!!)
-        .exportToFile("src/main/resources/results/i${config.instanceId}-${formattedTimestamp}.txt")
+        .exportToFile("src/main/resources/results/i${config.instanceId}-${formattedTimestamp}-${Random.nextLong()}.txt")
 
 //    File("src/main/resources/results/i${config.instanceId}-$formattedTimestamp.json").writeText(Json.encodeToString(config))
 
