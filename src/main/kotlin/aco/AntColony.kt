0cb189b630c52278fd6aba32b60ffcf88e8d18f6
Ivan Navratil <ivan.navratil@porsche.digital>
2021-12-29 15:16:47 +0100

    SA update

--- src/main/kotlin/aco/AntColony.kt
+++ src/main/kotlin/aco/AntColony.kt
@@ -2,11 +2,13 @@ package aco
 
 import helpers.Config
 import helpers.Distances
+import helpers.seededRandom
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.jetbrains.bio.viktor.F64Array
 import shared.Instance
 import shared.Solution
+import kotlin.math.exp
 
 class AntColony(
     val instance: Instance,
@@ -24,8 +26,10 @@ class AntColony(
 
     private fun performSingleIteration(
         antConfig: Config.Ant
-    ): Ant.SolutionBuilder? {
+    ): Boolean {
         // TODO Simulated annealing
+
+        val pheromonesLocal = pheromones.copy()
 
         val solutions = mutableListOf<Ant.SolutionBuilder>()
         repeat(antConfig.count) {  // TODO parallelize or use local pheromones (ACS)
@@ -33,26 +37,72 @@ class AntColony(
             ant.traverse()?.let(solutions::add)
         }
 
-        // TODO e.g. pick best 2?
-        val bestSolution = solutions.minOfOrNull { it } ?: return null
+        //all ants failed to found a solution
+        if (solutions.isEmpty()) {
+            return false
+        }
 
-        val pheromoneDelta = 1.0 / bestSolution.totalDistance
-        pheromones *= (1 - antConfig.rho)
-        bestSolution.routes.forEach {
-            it.route.zipWithNext().forEach { pair ->
-                val id1 = pair.first.node.id
-                val id2 = pair.second.node.id
-                pheromones[id1, id2] += antConfig.rho * pheromoneDelta
+        val bestAnt = solutions.minOf { it }
+
+        //for the first iteration
+        if (incumbentSolution == null) {
+            logger.trace("Incumbent solution set: " + Solution.fromSolutionBuilder(bestAnt).formatOutput())
+            incumbentSolution = bestAnt
+        }
+
+        val q = seededRandom.nextDouble()
+
+        val antsThatCanLayPheromones = solutions.filter {
+
+            val incumbentVehicles = incumbentSolution!!.vehiclesUsed
+            val incumbentDistance = incumbentSolution!!.totalDistance
+
+            val vehicleDifference = it.vehiclesUsed - incumbentVehicles
+            val distanceDifference = it.totalDistance - incumbentDistance
+
+            if (vehicleDifference < 0 || (vehicleDifference == 0 && distanceDifference < 0)) {
+                return true
             }
+
+            // želim VELIKI S za LOŠA rješenja
+
+            //     INCUMB     CAND
+            //npr 10,1000 vs 12,800 => tempD = 200 ; 800 + 200 * (2) * 0.2 =
+
+            //npr 10,1000 vs 11,800 => tempD = 200 ; 800 + 200 * (1) * 0.2 =
+
+            //npr 10,1000 vs 11,1500 => tempD = 200 ; 1500 + (-500) * (1) * 0.2 =
+
+            //npr 10,1000 vs 10,1500 => tempD = 200 ; 1500 + (-500) * 0 * 0.2 =
+
+            //npr 10,1000 vs 10,800 => tempD = 200 ; 800 + (200) * 0 * 0.2 =
+
+            val ds = distanceDifference * (1 + (vehicleDifference) * 0.2)
+
+            exp(-ds / currentTemperature) > q
         }
 
-        if (incumbentSolution == null || bestSolution < incumbentSolution!!) {
-            logger.info("Found new best solution - vehicles: ${bestSolution.vehiclesUsed}, distance: ${bestSolution.totalDistance}")
-            logger.trace(Solution.fromSolutionBuilder(bestSolution).formatOutput())
-            incumbentSolution = bestSolution
+        antsThatCanLayPheromones.forEach {
+
+            val pheromoneDelta = 1.0 / it.totalDistance
+            pheromones *= (1 - antConfig.rho)
+            it.routes.forEach { antTraversal ->
+                antTraversal.route.zipWithNext().forEach { pair ->
+                    val id1 = pair.first.node.id
+                    val id2 = pair.second.node.id
+                    pheromones[id1, id2] += antConfig.rho * pheromoneDelta
+                }
+            }
+
         }
 
-        return bestSolution
+        if (bestAnt < incumbentSolution!!) {
+            logger.info("Found new best solution - vehicles: ${bestAnt.vehiclesUsed}, distance: ${bestAnt.totalDistance}")
+            logger.trace(Solution.fromSolutionBuilder(bestAnt).formatOutput())
+            incumbentSolution = bestAnt
+        }
+
+        return true
     }
 
     fun run(config: Config) {
