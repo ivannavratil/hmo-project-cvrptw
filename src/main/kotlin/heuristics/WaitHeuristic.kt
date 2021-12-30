62e1e8f28bfd562273cab409bcdd8b45375437dc
LMesaric <luka.mesaric@fer.hr>
2021-12-30 14:40:33 +0100

    Cache travel times

--- src/main/kotlin/heuristics/WaitHeuristic.kt
+++ src/main/kotlin/heuristics/WaitHeuristic.kt
@@ -1,13 +1,13 @@
 package heuristics
 
-import helpers.Distances.calculateTravelTime
+import helpers.Distances.travelTime
 import shared.Node
 import shared.NodeMeta
 
 object WaitHeuristic {
 
     fun calculateWaitTime(sourceNodeMeta: NodeMeta, destination: Node): Double {
-        val arrivalTime = sourceNodeMeta.departureTime + calculateTravelTime(sourceNodeMeta.node.id, destination.id)
+        val arrivalTime = sourceNodeMeta.departureTime + travelTime[sourceNodeMeta.node.id, destination.id]
         return when {
             destination.readyTime <= arrivalTime -> 1.0
             else -> 1.0 / (destination.readyTime - arrivalTime)
