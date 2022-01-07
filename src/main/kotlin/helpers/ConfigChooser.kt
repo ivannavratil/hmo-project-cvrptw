af6e103b22a2b7a56661998d9e40baa274c93671
LMesaric <luka.mesaric@fer.hr>
2022-01-07 17:49:21 +0100

    Delegate config calls

--- src/main/kotlin/helpers/ConfigChooser.kt
+++ src/main/kotlin/helpers/ConfigChooser.kt
@@ -5,15 +5,19 @@ import com.sksamuel.hoplite.ConfigLoader
 object ConfigChooser {
 
     fun getConfig1m(instanceId: Int): Config {
-        return getConfigFromResource("/config-1m-i$instanceId.yaml")
+        return getConfig(instanceId, "1m")
     }
 
     fun getConfig5m(instanceId: Int): Config {
-        return getConfigFromResource("/config-5m-i$instanceId.yaml")
+        return getConfig(instanceId, "5m")
     }
 
     fun getConfigUnbounded(instanceId: Int): Config {
-        return getConfigFromResource("/config-un-i$instanceId.yaml")
+        return getConfig(instanceId, "un")
+    }
+
+    fun getConfig(instanceId: Int, timeLimitMarker: String): Config {
+        return getConfigFromResource("/config-$timeLimitMarker-i$instanceId.yaml")
     }
 
     private fun getConfigFromResource(resource: String): Config {
