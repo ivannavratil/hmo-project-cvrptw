129f129f79b6a0120e75b4950cf42b76220d27f3
Ivan Navratil <ivan.navratil@porsche.digital>
2022-01-07 20:45:34 +0100

    Useless log removed

--- src/main/kotlin/local/LocalSearch.kt
+++ src/main/kotlin/local/LocalSearch.kt
@@ -1,6 +1,12 @@
 package local
 
-import helpers.*
+import helpers.CompositeTermination
+import helpers.Config
+import helpers.ITerminationCriteria
+import helpers.TotalIterationsTermination
+import helpers.TotalTimeTermination
+import helpers.WeightedLottery
+import helpers.seededRandom
 import org.apache.logging.log4j.LogManager
 import shared.Instance
 import shared.NodeMeta
@@ -84,11 +90,11 @@ class LocalSearch(
             incumbentSolution = currentSolution
             incumbentEvaluations = evaluations
             incumbentTime = Duration.between(startTime, Instant.now())
-//            logger.info(
-//                "Found new best solution - " +
-//                        "vehicles: ${incumbentSolution.vehiclesUsed}, distance: ${incumbentSolution.totalDistance}, " +
-//                        "time: ${incumbentTime.toSeconds()}s, evaluations: $incumbentEvaluations"
-//            )
+            logger.info(
+                "Found new best solution - " +
+                        "vehicles: ${incumbentSolution.vehiclesUsed}, distance: ${incumbentSolution.totalDistance}, " +
+                        "time: ${incumbentTime.toSeconds()}s, evaluations: $incumbentEvaluations"
+            )
         }
 //        logger.info("Iterations: $iters")
     }
@@ -159,7 +165,7 @@ class LocalSearch(
 
         val routeRemovals = findTwoOptRouteRemoval() + findNodeTransferImprovements(true)
         val bestSwap = if (routeRemovals.isNotEmpty()) {
-            logger.trace("Removed a vehicle via LS!!!")
+            //logger.trace("Removed a vehicle via LS!!!")
             routeRemovals.maxOrNull()!!
         } else {
             chooser()
