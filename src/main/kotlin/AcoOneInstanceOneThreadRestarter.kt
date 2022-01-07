b55f36099983e3c5cc9ce9f1a85eab248782a627
LMesaric <luka.mesaric@fer.hr>
2022-01-07 21:21:43 +0100

    Track global incumbent solution

--- src/main/kotlin/AcoOneInstanceOneThreadRestarter.kt
+++ src/main/kotlin/AcoOneInstanceOneThreadRestarter.kt
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.apache.logging.log4j.core.config.Configurator
 import shared.Instance
+import shared.SolutionBuilder
 
 fun main() {
     val logger: Logger = LogManager.getLogger("main")
@@ -16,7 +17,13 @@ fun main() {
     val instance = Instance.fromInstanceId(instanceId)
     val config = ConfigChooser.getConfig(instanceId, timeLimitMarker)
 
+    var incumbent: SolutionBuilder? = null
+
     while (true) {
-        searchWithExports(instance, config.deepCopy(), exportName, logger)
+        val foundSolution = searchWithExports(instance, config.deepCopy(), exportName, logger)
+        if (incumbent == null || foundSolution != null && foundSolution < incumbent) {
+            incumbent = foundSolution
+            logger.info("GLOBAL BEST CHANGED: - vehicles: ${incumbent!!.vehiclesUsed}, distance: ${incumbent.totalDistance}")
+        }
     }
 }
