4452c67b00ea3660e7d9d327479373643965eb66
Ivan Navratil <ivan.navratil@porsche.digital>
2022-01-08 23:13:26 +0100

    New params

--- src/main/kotlin/AcoAllInstancesMultipleThreadsParameterVariations.kt
+++ src/main/kotlin/AcoAllInstancesMultipleThreadsParameterVariations.kt
@@ -21,43 +21,43 @@ fun main() {
 
             val alpha = thread(name = "alpha") {
                 val cAlpha = baseConfig.deepCopy()
-                cAlpha.ant.alpha = Random().nextInt(0, 20).toDouble() / 10
+                cAlpha.ant.alpha = Random().nextInt(0, 21).toDouble() / 10
                 searchWithExports(instance, cAlpha, "$exportName-alpha", logger, cAlpha.ant.alpha)
             }
 
             val beta = thread(name = "beta") {
                 val cBeta = baseConfig.deepCopy()
-                cBeta.ant.beta = Random().nextInt(0, 20).toDouble() / 10
+                cBeta.ant.beta = Random().nextInt(0, 21).toDouble() / 10
                 searchWithExports(instance, cBeta, "$exportName-beta", logger, cBeta.ant.beta)
             }
 
             val count = thread(name = "count") {
                 val cCount = baseConfig.deepCopy()
-                cCount.ant.count = Random().nextInt(1, 20) * 5
+                cCount.ant.count = Random().nextInt(1, 11) * 10
                 searchWithExports(instance, cCount, "$exportName-count", logger, cCount.ant.count)
             }
 
             val q0 = thread(name = "q0") {
                 val cq0 = baseConfig.deepCopy()
-                cq0.ant.q0 = Random().nextInt(0, 20).toDouble() / 10
+                cq0.ant.q0 = Random().nextInt(0, 21).toDouble() / 10
                 searchWithExports(instance, cq0, "$exportName-q0", logger, cq0.ant.q0)
             }
 
             val rho = thread(name = "rho") {
                 val cRho = baseConfig.deepCopy()
-                cRho.ant.rho = Random().nextInt(0, 10).toDouble() / 100
+                cRho.ant.rho = Random().nextInt(0, 11).toDouble() / 100
                 searchWithExports(instance, cRho, "$exportName-rho", logger, cRho.ant.rho)
             }
 
             val tau = thread(name = "tau") {
                 val cTau = baseConfig.deepCopy()
-                cTau.antColony.tauZero = 1.0.pow(Random().nextInt(-8, -1).toDouble())
+                cTau.antColony.tauZero = 1.0.pow(Random().nextInt(-8, 0).toDouble())
                 searchWithExports(instance, cTau, "$exportName-tau", logger, cTau.antColony.tauZero)
             }
 
             val theta = thread(name = "theta") {
                 val cTheta = baseConfig.deepCopy()
-                cTheta.ant.theta = Random().nextInt(0, 10).toDouble() / 10
+                cTheta.ant.theta = Random().nextInt(0, 11).toDouble() / 10
                 searchWithExports(instance, cTheta, "$exportName-theta", logger, cTheta.ant.theta)
             }
 
