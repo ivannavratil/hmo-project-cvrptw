f63e611af87da77ea327a8615375aca9f82acbe8
LMesaric <luka.mesaric@fer.hr>
2022-01-08 23:57:47 +0100

    Convert to thread pool

--- src/main/kotlin/AcoAllInstancesMultipleThreadsParameterVariations.kt
+++ src/main/kotlin/AcoAllInstancesMultipleThreadsParameterVariations.kt
@@ -2,75 +2,74 @@ import helpers.ConfigChooser
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import shared.Instance
-import java.util.Random
-import kotlin.concurrent.thread
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadLocalRandom
+import java.util.concurrent.ThreadPoolExecutor
 import kotlin.math.pow
 
 fun main() {
     val logger: Logger = LogManager.getLogger("main")
 
+    val timeLimitMarker = "5m"
+
+    val exportNames = (1..6).associateWith { "res-$timeLimitMarker-i$it" }
+    val instances = (1..6).associateWith { Instance.fromInstanceId(it) }
+    val baseConfigs = (1..6).associateWith { ConfigChooser.getConfig(it, timeLimitMarker) }
+
+    val executor = Executors.newFixedThreadPool(6) as ThreadPoolExecutor
+
     while (true) {
+        if (executor.queue.size > 50) {
+            Thread.sleep(60 * 1000)
+            continue
+        }
 
         for (instanceId in 1..6) {
+            val exportName = exportNames[instanceId]!!
+            val instance = instances[instanceId]!!
+            val baseConfig = baseConfigs[instanceId]!!
 
-            val timeLimitMarker = "5m"
-            val exportName = "res-$timeLimitMarker-i$instanceId"
-
-            val instance = Instance.fromInstanceId(instanceId)
-            val baseConfig = ConfigChooser.getConfig(instanceId, timeLimitMarker)
-
-            val alpha = thread(name = "alpha") {
+            executor.execute {
                 val cAlpha = baseConfig.deepCopy()
-                cAlpha.ant.alpha = Random().nextInt(0, 21).toDouble() / 10
+                cAlpha.ant.alpha = ThreadLocalRandom.current().nextInt(0, 21).toDouble() / 10
                 searchWithExports(instance, cAlpha, "$exportName-alpha", logger, cAlpha.ant.alpha)
             }
 
-            val beta = thread(name = "beta") {
+            executor.execute {
                 val cBeta = baseConfig.deepCopy()
-                cBeta.ant.beta = Random().nextInt(0, 21).toDouble() / 10
+                cBeta.ant.beta = ThreadLocalRandom.current().nextInt(0, 21).toDouble() / 10
                 searchWithExports(instance, cBeta, "$exportName-beta", logger, cBeta.ant.beta)
             }
 
-            val count = thread(name = "count") {
+            executor.execute {
                 val cCount = baseConfig.deepCopy()
-                cCount.ant.count = Random().nextInt(1, 11) * 10
+                cCount.ant.count = ThreadLocalRandom.current().nextInt(1, 11) * 10
                 searchWithExports(instance, cCount, "$exportName-count", logger, cCount.ant.count)
             }
 
-            val q0 = thread(name = "q0") {
+            executor.execute {
                 val cq0 = baseConfig.deepCopy()
-                cq0.ant.q0 = Random().nextInt(0, 21).toDouble() / 10
+                cq0.ant.q0 = ThreadLocalRandom.current().nextInt(0, 11).toDouble() / 10
                 searchWithExports(instance, cq0, "$exportName-q0", logger, cq0.ant.q0)
             }
 
-            val rho = thread(name = "rho") {
+            executor.execute {
                 val cRho = baseConfig.deepCopy()
-                cRho.ant.rho = Random().nextInt(0, 11).toDouble() / 100
+                cRho.ant.rho = ThreadLocalRandom.current().nextInt(0, 21).toDouble() * 4 / 100
                 searchWithExports(instance, cRho, "$exportName-rho", logger, cRho.ant.rho)
             }
 
-            val tau = thread(name = "tau") {
+            executor.execute {
                 val cTau = baseConfig.deepCopy()
-                cTau.antColony.tauZero = 1.0.pow(Random().nextInt(-8, 0).toDouble())
+                cTau.antColony.tauZero = 1.0.pow(ThreadLocalRandom.current().nextInt(-8, 0).toDouble())
                 searchWithExports(instance, cTau, "$exportName-tau", logger, cTau.antColony.tauZero)
             }
 
-            val theta = thread(name = "theta") {
+            executor.execute {
                 val cTheta = baseConfig.deepCopy()
-                cTheta.ant.theta = Random().nextInt(0, 11).toDouble() / 10
+                cTheta.ant.theta = ThreadLocalRandom.current().nextInt(0, 11).toDouble() / 10
                 searchWithExports(instance, cTheta, "$exportName-theta", logger, cTheta.ant.theta)
             }
-
-            alpha.join()
-            beta.join()
-            count.join()
-            q0.join()
-            rho.join()
-            tau.join()
-            theta.join()
-
-            logger.info("All threads restarting!")
         }
     }
 }
-
